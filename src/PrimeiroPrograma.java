f8065ecd5491d728b886de50d8946f6048fb5793
euqirneh03 <101140031+euqirneh03@users.noreply.github.com>
2023-03-20 23:50:47 -0300

    first commit

--- src/PrimeiroPrograma.java
+++ src/PrimeiroPrograma.java
@@ -7,9 +7,46 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro(nome: "Oproblema dos 3 corpos", numPaginas: 300);
+        boolean Livro1;
+        System.out.println(Livro1);
         /*int a = 12;
         int b = 6;
 
         System.out.println("Hello World!" + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
