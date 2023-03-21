16ecb2e24fd2b9e7bc29235a498973c382ac24fd
euqirneh03 <101140031+euqirneh03@users.noreply.github.com>
2023-03-20 23:30:12 -0300

    first commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 12;
+        int b = 6;
+
+        System.out.println("Hello World!" + (a+b));*/
+    }
+}
